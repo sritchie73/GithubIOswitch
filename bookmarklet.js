fc704dbd607625deea96b7545e8b955a8a4ffc79
Scott Ritchie <sritchie73@gmail.com>
2015-06-01 14:58:53 +1000

    Fixed bug with URLs ending with "/"

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,7 +5,7 @@ javascript:(function(){
   var host = window.location.host;
   if (host === "github.com") {
     var repo = path.split('/')[1];
-    if (ps.length === 3) {
+    if ((ps.length === 4 && f === "") || (ps.length ===3)) {
       window.location.href = "http://" + repo + ".github.io/" + path.replace("/" + repo + "/", "")
     } else if (ps.length > 3 && f.split(".").length === 2) {
       var newpath = path.replace("/" + repo + "/", "").replace("blob/gh-pages/", "");
@@ -13,7 +13,7 @@ javascript:(function(){
     }
   } else if (host.split('.')[1] === "github" && host.split('.')[2] === "io") {
     var repo = host.split('.')[0];
-    if (ps.length === 2) {
+    if ((ps.length === 3 && f === "") || (ps.length === 2)) {
       window.location.href = "http://github.com" + "/" + repo + path
     } else if (ps.length > 2) {
       var newpath = path.replace(f, "") + "blob/gh-pages/" + f;
